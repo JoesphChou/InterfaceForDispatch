50119dd7850755e5444eb0bf05945bfc711e6a1d
Joseph <036303@dragonsteel.com.tw>
2025-02-16 19:52:56 +0800

    initial main.py

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ import pandas as pd
 import time, math
 import urllib3
 from bs4 import BeautifulSoup
-# test git funtion
-# test change sychronized to github
-# test new branch
-# one more test
+
 def timeit(func):
     print('接到 func', func.__name__)
     def wrapper(*args, **kwargs):
